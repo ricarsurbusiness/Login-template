b2afd12d2efd0b72357d9a6117b18bd92980858a
Ricardo Suarez <rjosesuarez@unicesar.edu.co>
2025-07-14 02:37:33 -0500

    feat:creation of 'loginTemplate', 'globalStyles', 'responsiveTypes' and integration of tailwind for the creation of styles

---
+++ src/modules/auth/Login.tsx
@@ -0,0 +1,5 @@
+import { LoginTemplate } from "./components/index.ts";
+
+export function Login() {
+  return <LoginTemplate />;
+}
---
+++ src/style-g/globalStyles.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #f5f5f5;
+    font-family: 'Poppins', sans-serif;
+    color: blue;
+  }
+`;
